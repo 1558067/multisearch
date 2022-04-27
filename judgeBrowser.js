629271312d7ede1aaf896e07bf31249f76ba4a35
1558067 <97866782+1558067@users.noreply.github.com>
2022-04-27 20:21:26 +0900

    first commit

---
+++ judgeBrowser.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var userAgent = window.navigator.userAgent.toLowerCase();
+
+if (userAgent.indexOf('msie') != -1 ||
+    userAgent.indexOf('trident') != -1) {
+    window.alert("IEでは動作しませんので、GoogleChorme もしくは MicrosoftEdge からアクセスしてください");
+}
