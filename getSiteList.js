99928c6cbbf4623f291aa0aeaee413ec08b2e59d
1558067 <97866782+1558067@users.noreply.github.com>
2022-07-16 13:55:48 +0900

    add getSiteList.js

---
+++ getSiteList.js
@@ -0,0 +1,58 @@
+function getSiteList() {
+  return [
+    [
+      "T-Wave",
+      "http://nt-wave.mx.toyota.co.jp/tmc/twsearch/Pages/results.amainx?k=%%",
+    ],
+    ["資料検索(Office365)", "https://www.office.com/search?auth=2&q=%%"],
+    [
+      "Outlook&Teams一括検索",
+      "https://www.office.com/search/conversations?auth=2&q=%%",
+    ],
+    [
+      "OneDrive",
+      "https://toyotajp-my.sharepoint.com/_layouts/15/onedrive.amainx?view=7&searchScope=all&q=%%",
+    ],
+    ["Teams", "https://teams.microsoft.com/_#/apps/search?q=%%"],
+    [
+      "SharePoint",
+      "https://toyotajp.sharepoint.com.mcas.ms/_layouts/15/sharepoint.amainx?q=%%&v=search",
+    ],
+    ["Yammer", "https://web.yammer.com/main/search/threads?search=%%"],
+    [
+      "トヨタITサービスマネジメント",
+      "https://toyota1.service-now.com/main?id=search&q=%%",
+    ],
+    [
+      "従業員情報検索(Delve)",
+      "https://jpn.delve.office.com/?q=%%&searchpage=1&searchview=people&v=search",
+    ],
+    ["社内Bing", "https://www.bing.com/work/search?q=%%"],
+    [
+      "社内動画",
+      "https://web.microsoftstream.com/browse?q=%%&referrer=https:%2F%2Fpa-static-ms.azureedge.net%2F",
+    ],
+    [
+      "Toyota Wiki",
+      "http://toyotawiki.au.toyota.co.jp/wiki/index.php?search=%%",
+    ],
+    [
+      "T-Binder(ページ遷移のみ)",
+      "http://doc-lib17-tb.au.toyota.co.jp/TB5_Try/BinderLibrary/default.amainx",
+    ],
+    [
+      "T-Click(ページ遷移のみ)",
+      "https://t-click.kitora.toyota.co.jp/AgileWorks/SSO/picus.jmain",
+    ],
+    [
+      "ToyotaSearcher(ページ遷移のみ)",
+      "http://nt-wave.mx.toyota.co.jp/tmc/2/fssearch/Wiki/2_dounyu/UrlList.amainx",
+    ],
+    ["いらすとや", "https://www.irasutoya.com/search?q=%%"],
+    ["Flickr", "http://www.flickr.com/search/?q=%%"],
+    // ["Wikipedia", "http://ja.wikipedia.org/wiki/%%"],
+    // ["英辞郎 ", "http://eow.alc.co.jp/%%/UTF-8/"],
+    // ["Amazon", "http://www.amazon.co.jp/exec/obidos/external-search/?keyword=%%"],
+    // ["Youtube", "http://jp.youtube.com/results?search_query=%%"],
+  ];
+}
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let Doc = document,
+const Doc = document,
   body = Doc.getElementsByTagName("body")[0];
 function mainPanel(id) {
   let panel, sitesListArea, headerName;
@@ -14,6 +14,7 @@ function mainPanel(id) {
   panel.content = sitesListArea;
   return panel;
 }
+
 let t =
   "" +
   (window.getSelection
@@ -33,59 +34,9 @@ if (!t) {
   )
     t = decodeURIComponent(RegExp.$2);
 }
-let sitesArray = [
-  [
-    "T-Wave",
-    "http://nt-wave.mx.toyota.co.jp/tmc/twsearch/Pages/results.amainx?k=%%",
-  ],
-  ["資料検索(Office365)", "https://www.office.com/search?auth=2&q=%%"],
-  [
-    "Outlook&Teams一括検索",
-    "https://www.office.com/search/conversations?auth=2&q=%%",
-  ],
-  [
-    "OneDrive",
-    "https://toyotajp-my.sharepoint.com/_layouts/15/onedrive.amainx?view=7&searchScope=all&q=%%",
-  ],
-  ["Teams", "https://teams.microsoft.com/_#/apps/search?q=%%"],
-  [
-    "SharePoint",
-    "https://toyotajp.sharepoint.com.mcas.ms/_layouts/15/sharepoint.amainx?q=%%&v=search",
-  ],
-  ["Yammer", "https://web.yammer.com/main/search/threads?search=%%"],
-  [
-    "トヨタITサービスマネジメント",
-    "https://toyota1.service-now.com/main?id=search&q=%%",
-  ],
-  [
-    "従業員情報検索(Delve)",
-    "https://jpn.delve.office.com/?q=%%&searchpage=1&searchview=people&v=search",
-  ],
-  ["社内Bing", "https://www.bing.com/work/search?q=%%"],
-  [
-    "社内動画",
-    "https://web.microsoftstream.com/browse?q=%%&referrer=https:%2F%2Fpa-static-ms.azureedge.net%2F",
-  ],
-  ["Toyota Wiki", "http://toyotawiki.au.toyota.co.jp/wiki/index.php?search=%%"],
-  [
-    "T-Binder(ページ遷移のみ)",
-    "http://doc-lib17-tb.au.toyota.co.jp/TB5_Try/BinderLibrary/default.amainx",
-  ],
-  [
-    "T-Click(ページ遷移のみ)",
-    "https://t-click.kitora.toyota.co.jp/AgileWorks/SSO/picus.jmain",
-  ],
-  [
-    "ToyotaSearcher(ページ遷移のみ)",
-    "http://nt-wave.mx.toyota.co.jp/tmc/2/fssearch/Wiki/2_dounyu/UrlList.amainx",
-  ],
-  ["いらすとや", "https://www.irasutoya.com/search?q=%%"],
-  ["Flickr", "http://www.flickr.com/search/?q=%%"],
-  // ["Wikipedia", "http://ja.wikipedia.org/wiki/%%"],
-  // ["英辞郎 ", "http://eow.alc.co.jp/%%/UTF-8/"],
-  // ["Amazon", "http://www.amazon.co.jp/exec/obidos/external-search/?keyword=%%"],
-  // ["Youtube", "http://jp.youtube.com/results?search_query=%%"],
-];
+
+let sitesArray = getSiteList();
+
 let main = mainPanel("main");
 let mainContent = main.content,
   inputArea,
@@ -93,7 +44,6 @@ let mainContent = main.content,
   sitesTitle = [],
   i,
   searchButton,
-  tmp,
   form,
   checkbox;
 mainContent.appendChild((form = Doc.createElement("form")));
