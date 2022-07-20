b62c0d3e0b417593209e974c7f61c34fb396fb5c
1558067 <97866782+1558067@users.noreply.github.com>
2022-07-20 09:34:56 +0900

    refactoring

--- js/judgeBrowser.js
+++ js/judgeBrowser.js
@@ -1,8 +1,9 @@
-'use strict'
+"use strict";
 
 var userAgent = window.navigator.userAgent.toLowerCase();
 
-if (userAgent.indexOf('msie') != -1 ||
-    userAgent.indexOf('trident') != -1) {
-    window.alert("IEでは動作しませんので、GoogleChorme もしくは MicrosoftEdge からアクセスしてください");
+if (userAgent.indexOf("msie") != -1 || userAgent.indexOf("trident") != -1) {
+  window.alert(
+    "IEでは動作しませんので、GoogleChorme もしくは MicrosoftEdge からアクセスしてください"
+  );
 }
